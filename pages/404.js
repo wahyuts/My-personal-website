407e9f98ee3e2c52105d5113be435823d017e106
Wahyu Trisna <wahyu.trisna100@gmail.com>
2021-08-08 22:33:03 +0700

    update 404

---
+++ pages/404.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import {useEffect} from 'react'
+import {useRouter} from 'next/router'
+
+const NotFound = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            // router.go(-1)  ini untuk perintah back
+            // router.go(1)  ini untuk perintah forward
+            router.push('/'); // ini untuk perintah redirect langsung ke halaman ...
+        },3000)
+    },[])
+    return ( 
+        <div>
+            <div className="cont404">
+                <div className="not-found">
+                    <h1>Oooops...</h1>
+                    <h2>The page cannot be found</h2>
+                    <p>Go back to the <Link href="/"><a>Homepage</a></Link></p>
+                </div> 
+            </div>
+        </div>
+    
+    );
+}
+ 
+export default NotFound;
