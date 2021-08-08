af19f6db7e16b15eb17a72e9e2550e1103c7d651
Wahyu Trisna <wahyu.trisna100@gmail.com>
2021-08-08 22:12:06 +0700

    update malam

--- pages/Portfolio.js
+++ pages/Portfolio.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import styles from '../styles/Portfolio.module.css'
 
 const Portfolio = () => {
@@ -7,21 +8,29 @@ const Portfolio = () => {
                 <h2>My Portfolio</h2>
             </div>
             <div className={styles.gridWrapper}>
-                <div className={styles.zone}>
-                    <img src="/My-personal-web.png" width={300} height={185} style={{marginBottom:8}}/>
-                    <p>My personal web</p>
+                <div>
+                    <a href="https://github.com/wahyuts/My-personal-website" className={styles.zone}>
+                        <img src="/My-personal-web.png" width={300} height={185} style={{marginBottom:8}}/>
+                        <p>My personal web</p>
+                    </a>
                 </div>
-                <div className={styles.zone}>
-                    <img src="/comingsoon.jpg" width={300} height={185} style={{marginBottom:8}}/>
-                    <p>Stay tuned, coming soon!</p>
+                <div>
+                    <a href="https://simple-next-app-one.vercel.app/" className={styles.zone}>
+                        <img src="/Ninja-List.png" width={300} height={185} style={{marginBottom:8}}/>
+                        <p>Ninja-List (Tutor NextJS)</p>
+                    </a>
                 </div>
-                <div className={styles.zone}>
-                    <img src="/comingsoon.jpg" width={300} height={185} style={{marginBottom:8}}/>
-                    <p>Stay tuned, coming soon!</p>
+                <div>
+                    <a href="https://github.com/wahyuts/Travelook" className={styles.zone}>
+                        <img src="/Travelook.png" width={300} height={185} style={{marginBottom:8}}/>
+                        <p>Travelook</p>
+                    </a>
                 </div>
-                <div className={styles.zone}>
-                    <img src="/comingsoon.jpg" width={300} height={185} style={{marginBottom:8}}/>
-                    <p>Stay tuned, coming soon!</p>
+                <div>
+                    <a href="#" className={styles.zone}>
+                        <img src="/I-Look-project.png" width={300} height={185} style={{marginBottom:8}}/>
+                        <p>I-Look (on going)</p>
+                    </a>
                 </div>
                 <div className={styles.zone}>
                     <img src="/comingsoon.jpg" width={300} height={185} style={{marginBottom:8}}/>
